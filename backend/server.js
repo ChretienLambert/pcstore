d155e372e3375337ea8fb7a106471ec4c968af2d
ChretienLambert <wandjichretien@gmail.com>
2025-08-26 13:12:28 +0100

    feat: enhance product model and routes with additional fields and validation

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,22 @@
 const express = require('express');
 const cors = require('cors');
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
-
-
-const app = express();
-app.use(cors());
-app.use(express.json());
+const productRoutes = require('./routes/productRoutes');
 
 dotenv.config();
-
-const PORT = process.env.PORT || 3000;
-
-// Connect to MongoDB
 connectDB();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+const app = express();
 
-//API routes
-app.use("/api/users/", userRoutes)
+app.use(cors());
+// parse JSON and urlencoded bodies BEFORE your routes
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+app.use('/api/users', userRoutes);
+app.use('/api/products', productRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
